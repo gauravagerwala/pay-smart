80a500bdca4269c8a92483e1f326f5c80ae4e908
Gaurav <gauravagerwala@gmail.com>
2016-03-13 17:12:55 +0530

    Added user model

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 var express = require('express');
+// var db = require('./db.js');
+
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req,res) {
+	res.send('pay-smart API root');
+});
+
+db.sequelize.sync().then( function () {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT);
+	});
+});
 
---
+++ models/manager.js
@@ -0,0 +1,45 @@
+var bcrypt = require('bcrypt');
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('user',{
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true
+			validate: {
+				isEmail: true
+			}
+		}
+		type: {
+			type: DataTypes.STRING,
+			validate: {
+				isIn: [['customer','employee','manager']]
+			}
+		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
+		password: {
+			type: DataTypes.VIRTUAl,
+			allowNull: false
+			validate: {
+				len: [7,100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hasedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hasedPassword);
+			}
+		}
+	});	
+};
